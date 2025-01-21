37704578580c1294b08b1be4a8fa8f72c17cb45a
zahin <zahinhosen@gmail.com>
2025-01-21 22:23:38 +0600

    currently working

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Benefits from './components/Benefits'
+import Collaboration from './components/Collaboration'
+import Services from './components/Services'
+import Pricing from './components/Pricing'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Benefits/>
+    <Collaboration/>
+    <Services/>
+    <Pricing/>
+    </>
+  )
+}
+
+export default App
